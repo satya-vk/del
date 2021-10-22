aa8ad04ea07c3cd727cf3cec804154fd7cf37782
satyavk <n160241@rguktn.ac.in>
2021-10-23 01:31:49 +0530

    committed

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const app=express();
+app.set('view engine','ejs')
+app.use(express.static("public"));
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+app.get("/Janatagarege",function(req,res){
+  res.render("list",{title:"Janathagarege"})
+});
+app.get("/NannakuPrematho",function(req,res){
+  res.render("list",{title:"NannakuPrematho"})
+});
+app.get("/Jailvakusa",function(req,res){
+  res.render("list",{title:"Jailvakusa"});
+});
+
+
+app.listen(3000,function(){
+  console.log("server is started");
+});
